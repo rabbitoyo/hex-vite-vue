bf528d10256a1bc75114fb533d7b9de6db361393
rabbit <night31916@gmail.com>
2025-08-26 12:21:38 +0800

    add vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const num = ref(1)
+console.log(num.value)
+/*setInterval(() => {
+num.value++
+}, 500);*/
+
+function clickMe() {
+    num.value++
+}
+</script>
+
+<template>
+    <div>
+        {{ num }}
+        <button type="button" v-on:click="clickMe">點擊</button>
+    </div>
+</template>
+
+<style scoped></style>
